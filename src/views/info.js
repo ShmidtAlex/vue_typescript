7dfaf93d650a24f62ffaf0808ab3874b92d393c9
ShmidtAlex <shmidt.alex@gmail.com>
2020-02-23 02:44:47 +0300

    info.js file added (description for imitation API request

---
+++ src/views/info.js
@@ -0,0 +1,4 @@
+//for checking http requests and simulation of db interaction
+//1.created file db.json
+//2.install json-server : npm i -g json-server
+//3. use json server by command: json-server src/assets/db.json
