902310d89a4a0f8f9dddbc720f39dafe20061abc
ShmidtAlex <shmidt.alex@gmail.com>
2020-02-22 21:47:28 +0300

    bug with parsing json data fixed

--- src/views/About.vue
+++ src/views/About.vue
@@ -20,27 +20,27 @@
   export default class About extends Vue {
     //"private" means, that the variable or method is not available for other components
     private blogPosts: Post[] = [
-      {
-        title: 'My first blogpost ever!',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad deleniti dolorem labore, nihil possimus similique?',
-        author: 'Alexander',
-        datePosted: new Date(2019,1,18),
-        highlighted: false
-      },
-      {
-        title: 'Look, I am blogging!',
-        body: ' Animi aut consequatur illum incidunt mollitia, non rerum vitae. Aliquam aut commodi possimus ratione!',
-        author: 'Alexander',
-        datePosted: new Date(2019,1,20),
-        highlighted: true
-      },
-      {
-        title: 'Another one??',
-        body: 'oops, there is no lorem ipsum',
-        author: 'Annonimus',
-        datePosted: new Date(2019,1,22),
-        highlighted: false
-      }
+      // {
+      //   title: 'My first blogpost ever!',
+      //   body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad deleniti dolorem labore, nihil possimus similique?',
+      //   author: 'Alexander',
+      //   datePosted: new Date(2019,1,18),
+      //   highlighted: false
+      // },
+      // {
+      //   title: 'Look, I am blogging!',
+      //   body: ' Animi aut consequatur illum incidunt mollitia, non rerum vitae. Aliquam aut commodi possimus ratione!',
+      //   author: 'Alexander',
+      //   datePosted: new Date(2019,1,20),
+      //   highlighted: true
+      // },
+      // {
+      //   title: 'Another one??',
+      //   body: 'oops, there is no lorem ipsum',
+      //   author: 'Annonimus',
+      //   datePosted: new Date(2019,1,22),
+      //   highlighted: false
+      // }
     ]
     public showHighlighted: boolean = true;
     get visibleBlogPosts() {// getter here is an equivalent of computed property in vue.js
@@ -53,13 +53,13 @@
       //1.we can limit the amount of time the user has to wait for appearance of data on the screen
       //2.mounted hook is not used, while serverside rendering
       console.log('the app is created');
-      this.$http.get('http//localhost:8080/blogposts').then((response: AxiosResponse) => {
+      this.$http.get('http://localhost:3000/blogPosts').then((response: AxiosResponse) => {
         this.blogPosts = response.data.map((val: any) => ({
           title: val.title,
           body: val.body,
           author: val.author,
           datePosted: new Date(val.datePosted),
-          highlighted: val.highlighted,
+          highlighted: JSON.parse(val.highlighted),
         }));
       });
     }
